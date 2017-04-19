8c841addfb3964141747793bf794136a7f57b417
Ryan Gao <rgao14@gmail.com>
2017-04-19 00:32:33 -0700

    Challenges 3 and 4

--- cryptopals.py
+++ cryptopals.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import *
+import pandas as pd
 
 # 1.1
 import base64
@@ -16,6 +17,7 @@ def hex_to_base64(h):
     '''Converts hex-encoded string to base64-encoded string'''
     return bytes_to_base64(hex_to_bytes(h))
 
+
 # 1.2
 def fixed_xor(b1, b2):
     '''Produces the XOR combination of two equal-length byte arrays'''
@@ -30,3 +32,50 @@ def fixed_xor_hex(h1, h2):
 def bytes_to_hex(b):
     '''Converts byte array to hex-encoded string'''
     return ''.join('%02x' % byte for byte in b)
+
+
+# 1.3
+def single_byte_xor(bs, b):
+    '''Produces the XOR combination of a byte array with a single byte'''
+    return fixed_xor(bs, bytearray([b]*len(bs)))
+
+df = pd.read_csv('Data/LetterFrequency.tsv', delimiter='\t')
+frequencies = {key: value for key, value in zip(df.Letter, df.Frequency)}
+
+def english_score_byte(b):
+    '''Scores an ascii-encoded byte, based on English letter frequencies.'''
+    c = chr(b)
+    if c == ' ':
+        return log10(1/5.1) # the average English word is 5.1 letters long
+    
+    if str.isalpha(c):
+        return frequencies[c.lower()]
+    
+    # nonstandard ascii characters
+    if b < 32 or b > 126:
+        return log10(1e-10)
+    
+    return log10(1e-6)
+
+def english_score(bs):
+    '''Scores an ascii-encoded byte array, based on English letter frequencies.'''
+    return sum([english_score_byte(b) for b in bs])
+
+def bytes_to_ascii(b):
+    '''Converts byte array to ascii-encoded string'''
+    return b.decode('ascii')
+
+def decrypt_single_byte_xor(bs):
+    '''Returns the most-likely key and decrypted text for a byte array encrypted with single byte XOR'''
+    return sorted([(key, single_byte_xor(bs, key)) for key in xrange(255)], \
+                  key = lambda x: english_score(x[1]), \
+                  reverse = 1)[0]
+
+
+# 1.4
+def find_single_byte_xor_encryption(byte_arrays):
+    '''Finds the index, original byte array, key, and decrypted string of the 
+    byte array most likely to have been encrypted with single byte XOR.'''
+    return sorted([(i, bs) + decrypt_single_byte_xor(bs) for i, bs in enumerate(byte_arrays)],
+                  key = lambda x: english_score(x[3]),
+                  reverse = 1)[0]
